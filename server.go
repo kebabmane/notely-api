d48ee1a0aad95c6330c84551db057e2706bdc0ae
kebabman <evans.rhys@gmail.com>
2018-02-09 21:14:38 +1100

    first commit

---
+++ server.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/kebabmane/notely-api/app"
+	"github.com/kebabmane/notely-api/controller"
+	"github.com/kebabmane/notely-api/middlewares"
+	"github.com/kebabmane/notely-api/model"
+	_ "github.com/lib/pq"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/rs/cors"
+	"github.com/zbindenren/negroni-prometheus"
+)
+
+func main() {
+	// load application configurations in not production
+
+	if os.Getenv("ENV") == "PRODUCTION" {
+		fmt.Println("your running in production, did you know that?")
+	} else {
+		fmt.Println("your running in dev/test, did you know that?")
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	// migrate the database
+	model.Init()
+
+	// create the logger
+	logger := logrus.New()
+
+	// CORS middleware setup
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedHeaders:   []string{"Access-Control-Allow-Origin", "Content-Type", "Origin", "Accept-Encoding", "Accept-Language", "Authorization"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "OPTIONS", "DELETE"},
+		AllowCredentials: true,
+	})
+
+	// set up router
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/", homeHandler)
+
+	r.HandleFunc("/health", controller.HealthFunction).Methods("GET")
+
+	r.Handle("/metrics", prometheus.Handler())
+
+	// s is a subrouter to handle question routes
+	api := r.PathPrefix("/v1").Subrouter()
+
+	// categories routes
+	api.HandleFunc("/categories/", controller.FetchAllCategories).Methods("GET")
+	api.HandleFunc("/categories/", controller.CreateCategory).Methods("POST")
+	api.HandleFunc("/categories/{id}", controller.FetchSingleCategory).Methods("GET")
+	api.HandleFunc("/categories/{id}", controller.UpdateCategory).Methods("PUT")
+
+	// muxRouter uses Negroni handles the middleware for authorization
+	muxRouter := http.NewServeMux()
+	muxRouter.Handle("/", r)
+	muxRouter.Handle("/api/", negroni.New(
+		negroni.HandlerFunc(middlewares.CheckJWT()),
+		negroni.Wrap(api),
+	))
+
+	// Negroni handles the middleware chaining with next
+	n := negroni.Classic()
+
+	m := negroniprometheus.NewMiddleware("notely-api")
+
+	// Use promethus for service stuff
+	n.Use(m)
+
+	// Use CORS
+	n.Use(c)
+
+	// handle routes with the muxRouter
+	n.UseHandler(muxRouter)
+
+	// start the server
+	address := fmt.Sprintf(":%v", os.Getenv("PORT"))
+	logger.Infof("server %v is started at %v\n", app.Version, address)
+	panic(http.ListenAndServe(address, handlers.RecoveryHandler()(n)))
+
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("{\"message\": \"Hello world\"}"))
+}
---
+++ vendor/github.com/kebabmane/tureloGo/model/model.go
@@ -0,0 +1,91 @@
+package model
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// declare DB
+var db *gorm.DB
+
+// Category data model
+type Category struct {
+	gorm.Model
+	CategoryName        string `gorm:"unique_index"`
+	CategoryImageURL    string
+	CategoryDescription string
+	FeedsCount          string
+	CategoryID          uint
+}
+
+// Feed data model
+type Feed struct {
+	gorm.Model
+	FeedName        string `gorm:"unique_index"`
+	FeedURL         string
+	FeedIcon        string
+	FeedsCount      string
+	LastFeteched    string
+	FeedDescription string
+	FeedImageURL    string
+	FeedLastUpdated time.Time
+	Categories      []Category
+	FeedEntry       []FeedEntry
+}
+
+// FeedEntry data model
+type FeedEntry struct {
+	gorm.Model
+	FeedEntryTitle            string `gorm:"unique_index"`
+	FeedEntryURL              string
+	FeedEntryPublished        string
+	FeedEntryAuthor           string
+	FeedEntryContent          string
+	FeedEntryContentSanitized string
+	FeedEntryLink             string
+	FeedID                    uint
+}
+
+// Seeding tables:
+var categories []Category = []Category{
+	Category{CategoryName: "technology", CategoryImageURL: "https://www.imore.com/sites/imore.com/files/styles/xlarge/public/field/image/2016/03/ipad-mini-ipad-air-ipad-pro-stack-snow-hero.jpg?itok=ir4jkST2", CategoryDescription: "this is where we put some technology stuff"},
+	Category{CategoryName: "health", CategoryImageURL: "https://lorempixel.com/600/300/food/5/", CategoryDescription: "this is where we put some health stuff"},
+	Category{CategoryName: "medical", CategoryImageURL: "https://lorempixel.com/600/300/food/5/", CategoryDescription: "this is where we put some medical stuff"},
+}
+
+var feeds []Feed = []Feed{
+	Feed{FeedName: "The Verge -  All Posts", FeedURL: "http://theverge.com/rss/index.xml", FeedDescription: "this is where we put some technology stuff", FeedIcon: "https://cdn.vox-cdn.com/community_logos/52801/VER_Logomark_32x32..png"},
+}
+
+// Init migrates the database, in the future add a feature flag to know when to migrate
+func Init() {
+
+	dbString := "postgres://postgres:postgres@127.0.0.1:5432/turelogo?sslmode=disable"
+
+	fmt.Println("Is this your DB string: ", dbString)
+	var err error
+	db, err = gorm.Open("postgres", dbString)
+	if err != nil {
+		panic("Unable to connect to DB")
+	}
+
+	db.AutoMigrate(&Category{})
+	db.AutoMigrate(&Feed{})
+	db.AutoMigrate(&FeedEntry{})
+	fmt.Println("We have migrated the database")
+
+	db.Unscoped().Delete(&categories)
+	db.Unscoped().Delete(&feeds)
+	fmt.Println("We have reset the database")
+
+	for _, category := range categories {
+		db.Create(&category)
+	}
+	for _, feed := range feeds {
+		db.Create(&feed)
+	}
+	fmt.Println("We have seeded the database with feeds & categories")
+}
